5642b13a64da2d9c5da15556e9dca6248c84aa36
ViniDalvino <vinidalvino@cock.li>
2021-02-23 09:52:16 -0500

    Refactored and fixed shit with bitwise or

--- src/WinApiWrapper.cpp
+++ src/WinApiWrapper.cpp
@@ -1,4 +1,4 @@
-#include <WinApiWrapper.h>
+#include "WinApiWrapper.h"
 #include <Oleacc.h>
 #include <algorithm>
 #include <Util.h>
@@ -27,7 +27,7 @@ std::vector<process> process::enumAllProcess()
     {
       if (!processScanned.th32ProcessID)
         continue;
-      if (std::wstring(processScanned.szExeFile).empty() || std::wstring(processScanned.szExeFile) == L"System" | std::wstring(processScanned.szExeFile) == L"svchost.exe")
+      if (std::wstring(processScanned.szExeFile).empty() || std::wstring(processScanned.szExeFile) == L"System" || std::wstring(processScanned.szExeFile) == L"svchost.exe")
         continue;
       processes.push_back(process(processScanned.dwSize, std::wstring(processScanned.szExeFile)));
     } while (Process32Next(hndl, &processScanned));
@@ -65,7 +65,11 @@ DWORD waitForProgramLaunchSync(const std::wstring toWatch)
     BOOST_FOREACH (process p, process::enumAllProcess())
     {
       if (p.name == toWatch)
+      {
+        s.stop();
+        std::cout << "Found the program!";
         return p.pid;
+      }
     }
   }
   s.stop();
@@ -81,49 +85,5 @@ void waitForProgramLaunchAsync(const std::wstring toWatch, std::function<void(DW
 
 InjectDllReturnValue InjectDll(DWORD pid, std::string Path)
 {
-  if (!std::filesystem::exists(Path))
-    return InjectDllReturnValue::InexistantDLL;
-  // Some top variables aha..
-  DWORD ProcessId = pid;
-  DWORD ExitCode; // Defining the exit code for the thread
-
-  long DLL_ = Path.length() + 1;
-
-  HANDLE ProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessId); // Getting the process handle
-  if (!ProcessHandle)
-  {
-    return InjectDllReturnValue::AcessWindowHandleError;
-  }
-  LPVOID MemAllocation = VirtualAllocEx(ProcessHandle, NULL, DLL_, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE); // Getting memory allocation
-  if (!MemAllocation)
-  {
-    return InjectDllReturnValue::MemAlocationError;
-  } // If didn't find the memory allocation it will return internal error, since that's what it is lool
-    /*
-	  Writing to memory
-	  We use virtual protect to change the protection region
-	*/
-  DWORD WriteMemOldProc;
-  VirtualProtect(WriteProcessMemory, 1, PAGE_EXECUTE_READWRITE, &WriteMemOldProc);
-  int WritingSuccessful = WriteProcessMemory(ProcessHandle, MemAllocation, utf8ToUtf16(Path.c_str()).c_str(), DLL_, 0);
-  VirtualProtect(WriteProcessMemory, 1, WriteMemOldProc, &WriteMemOldProc);
-  /*
-	  Creating a thread to the target handle
-	*/
-  LPTHREAD_START_ROUTINE Kernel32_Library = (LPTHREAD_START_ROUTINE)GetProcAddress(LoadLibraryW(L"kernel32"), "LoadLibraryW");               // Staring the routine
-  HANDLE DLLHandler_ = CreateRemoteThread(ProcessHandle, NULL, NULL, (LPTHREAD_START_ROUTINE)Kernel32_Library, MemAllocation, NULL, NULL); // Creating the thread
-  WaitForSingleObject(DLLHandler_, INFINITE);                                                                                              // Wating for the object
-  VirtualFreeEx(ProcessHandle, MemAllocation, NULL, MEM_RELEASE);                                                                          // Free the dll
-  GetExitCodeThread(DLLHandler_, &ExitCode);                                                                                               // Checking the exit code
-
-  // Checking if the injection was successful
-  CloseHandle(ProcessHandle); // Closing the process handle
-  if (!ExitCode)
-  {
-    return InjectDllReturnValue::InjectionFail;
-  }
-  else
-  {
-    return InjectDllReturnValue::InjectionSucess;
-  }
+  blackbone::dlls 
 }
